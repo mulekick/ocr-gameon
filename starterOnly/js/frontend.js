fa43deef0c8ae2cb37e0cb970c7469b751e45ecb
mulekick <karim_laouari@hotmail.com>
2022-07-09 01:12:13 +0200

    fixes + improvements + mobile layout

--- starterOnly/js/frontend.js
+++ starterOnly/js/frontend.js
@@ -4,76 +4,65 @@
 try {
 
     const
-        // elements list : mobile navigation icon, navigation menu, signup form, signup form modal popup, modal popup close button
-        eltsSelectors = [ `.icon`, `#myTopnav`, `form[name="reserve"]`, `.bground`, `.close` ],
+        // elements list : mobile navigation icon, navigation menu, signup form, signup form modal popup, modal popup close button, modal popup post submission close button
+        eltsSelectors = [ `.icon`, `#myTopnav`, `form[name="reserve"]`, `.bground`, `.close`, `#close-confirm` ],
         // retrieve DOM elements
-        [ navIcon, navMenu, signupForm, signupFormPopup, signupFormCloseButton ] = eltsSelectors.map(x => document.querySelector(x)),
+        [ navIcon, navMenu, signupForm, signupFormPopup, signupFormCloseButton, signupFormConfirmCloseButton ] = eltsSelectors.map(x => document.querySelector(x)),
         // signup buttons desktop + mobile
         signupButtons = document.querySelectorAll(`.modal-btn`),
         // form fields list
         fldsSelectors = [ `#first`, `#last`, `#email`, `#birthdate`, `#quantity`, `input[name="location"]`, `#checkbox1` ];
 
-    fldsSelectors
-        // reset error message on input
-        .forEach(x => document.querySelector(x).addEventListener(`change`, e => e.target.parentElement.removeAttribute(`data-error-visible`)));
-
-    signupForm
-        // manage form submission
-        .addEventListener(`submit`, e => {
-            try {
-
-                const
-                    // check form fields validity
-                    submit = fldsSelectors
-                        // ensure all error messages are displayed
-                        .reduce((r, x) => {
-                            const
-                                // use nested destructuring on all possible properties
-                                {parentElement, validity: {valueMissing, patternMismatch, typeMismatch, rangeUnderflow, rangeOverflow}} = document.querySelector(x),
-                                // current field validity state
-                                cfv = valueMissing || patternMismatch || typeMismatch ||  rangeUnderflow || rangeOverflow;
-                            // display error message or reset
-                            cfv ? parentElement.setAttribute(`data-error-visible`, `true`) : parentElement.removeAttribute(`data-error-visible`);
-                            // update accumulator and return
-                            return r && !cfv;
-                        // form is valid by default
-                        }, true);
-
-                if (submit === false)
-                    // throw error if vlidation failed
-                    throw new Error(`signup form validation failed`);
-
-                // bust modal popup contents and display a confirmation message
-                document.querySelector(`.modal-body`).innerHTML =
-                    `<div class="submit-confirm">
-                        <span>&nbsp;</span>
-                        <span>Merci pour votre inscription</span>
-                        <input type="button" id="close-confirm" class="btn-submit" value="Fermer" />
-                    </div>`;
-
-                document.querySelector(`#close-confirm`)
-                    // manage popup close button
-                    .addEventListener(`click`, () => (signupFormPopup.style.display = `none`));
-
-            } catch (err) {
-                // write to console
-                console.error(`error occured: ${ err.message }`);
-                // prevent form submission
-                e.preventDefault();
-            }
-        });
-
     navIcon
         // Toggle navigation menu on mobile
         .addEventListener(`click`, () => (navMenu.className = navMenu.className === `topnav` ? `topnav responsive` : `topnav`));
 
-    signupFormCloseButton
-        // manage popup close button
-        .addEventListener(`click`, () => (signupFormPopup.style.display = `none`));
+    fldsSelectors
+        // reset error message on input
+        .forEach(x => document.querySelector(x).addEventListener(`input`, e => e.target.parentElement.removeAttribute(`data-error-visible`)));
+
+    signupForm
+        // manage form submission
+        .addEventListener(`submit`, e => {
+            const
+                // check form fields validity
+                submit = fldsSelectors
+                    // ensure all error messages are displayed
+                    .reduce((r, x) => {
+                        const
+                            // use nested destructuring on all possible properties
+                            {parentElement, validity: {valueMissing, patternMismatch, typeMismatch, rangeUnderflow, rangeOverflow}} = document.querySelector(x),
+                            // current field validity state
+                            cfv = valueMissing || patternMismatch || typeMismatch ||  rangeUnderflow || rangeOverflow;
+                        // display error message or reset
+                        cfv ? parentElement.setAttribute(`data-error-visible`, `true`) : parentElement.removeAttribute(`data-error-visible`);
+                        // update accumulator and return
+                        return r && !cfv;
+                    // form is valid by default
+                    }, true);
+
+            if (submit === true)
+                // hide form and display the confirmation message
+                signupForm.style.display = `none`;
+
+            // prevent actual submission regardless of validity, ajax should be used to submit the form if the confirmation message is to be displayed weSmart
+            e.preventDefault();
+        });
+
+    [ signupFormCloseButton, signupFormConfirmCloseButton ]
+        // manage popup close buttons
+        .forEach(x => x.addEventListener(`click`, () => (signupFormPopup.style.display = `none`)));
 
     signupButtons
-        // show signup popup
-        .forEach(x => x.addEventListener(`click`, () => (signupFormPopup.style.display = `block`)));
+        // manage popup open buttons
+        .forEach(x => x.addEventListener(`click`, () => {
+            // reset form
+            signupForm.reset();
+            // display form
+            signupForm.style.display = `block`;
+            // display popup
+            signupFormPopup.style.display = `block`;
+        }));
 
 } catch (err) {
     // write to console
